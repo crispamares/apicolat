b1218f2e01781535941ac0a2dae8ad8836355429
Juan Morales <juan.morales@upm.es>
2014-07-04 15:53:00 +0200

    reworked run command

--- apicolat/app.py
+++ apicolat/app.py
@@ -16,8 +16,7 @@ from indyva.dynamics.dfilter import DynFilter
 import xlsx_exporter 
 
 from configuration import parseArgsAndConfig
-import random
-import socket
+from utils import get_random_port
 
 class MetaApp(object):
     def __init__(self):
@@ -52,35 +51,13 @@ class MetaApp(object):
         if config.use_random_port:
             port_range = (config.min_port, config.max_port)
             max_tries = config.port_max_tries
-            port = self._get_random_port(port_range=port_range, max_tries=max_tries)
+            port = get_random_port(port_range=port_range, max_tries=max_tries)
         elif default is not None:
             port = default
         else:
             raise Exception("Is impossible to get a free port")
 
-        return port
-
-    def _get_random_port(self, port_range, max_tries):
-        '''
-        Return a free port in a range
-
-        :param port_range: (int,int) The min and max port numbers
-        :param max_tries: The maximum number of bind attempts to make
-        :return port: int, the port ready to use
-        '''
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        for i in range(max_tries):
-            try:
-                port = random.randrange(port_range[0], port_range[1])
-                s.bind(('127.0.0.1',port))
-            except socket.error as e:
-                if not e.errno == socket.errno.EADDRINUSE:
-                    raise
-            else:
-                s.close()
-                return port
-        raise Exception("Could not find a free random port.")
-    
+        return port    
 
     def run(self):
         self.kernel.run_forever()
--- run.py
+++ run.py
@@ -5,28 +5,21 @@ import os, sys
 from circus import get_arbiter
 from circus.client import CircusClient
 
+from apicolat.utils import get_random_port
+
 ROOT = os.path.dirname(os.path.realpath(__file__))
 APICOLATDIR = os.path.join(ROOT, 'apicolat')
 WEBDIR = os.path.join(ROOT, 'apicolat', 'web')
+STATSDIR = os.path.join(ROOT, 'lib', 'indyva-r')
 
-#
-#  1.- Identify unused ports to be used by our commands
-#  
-#  2.- Spawn the processes
-#       If circusd is present:
-#           With ZMQ add new processes
-#       else:
-#           spawn the processes with subprocess module
-#
-
-zmq_port = 8085 
-ws_port = 8080
-web_port = 8001
+web_port = get_random_port((10000, 20000), 100)#8001
+zmq_port = get_random_port((10000, 20000), 100)
+starts_port = get_random_port((10000, 20000), 100)
 
 apicolat = {
     "cmd":  "python "+APICOLATDIR,
-    "name": "apicolat",
-    "args":  "--zmq_server --zmq_port {0} --ws_server --ws_port {1}".format(zmq_port, ws_port), 
+    "name": "apicolat-{0}".format(web_port),
+    "args":  "--zmq_server --zmq_port {0} --ws_server --ws_port {1} --web_dir {2}".format(zmq_port, web_port, WEBDIR), 
     "options": {
         "numprocesses": 1,
         "working_dir": ROOT,
@@ -34,31 +27,33 @@ apicolat = {
         }
     }
 
-web = {
-    "cmd":  "python -m SimpleHTTPServer {0}".format(web_port),
-    "name": "web",
+statistics = {
+    "cmd":  "R ",
+    "name": "apicolat-statistics-{0}".format(web_port),
+    "args":  "-f statistics.r --args {0} {1}".format(starts_port, zmq_port),
     "options": {
         "numprocesses": 1,
-        "working_dir": WEBDIR,
-        "copy_env": True
+        "working_dir": STATSDIR
         }
     }
 
-
+print('Starting in '+ STATSDIR, statistics['cmd'] + statistics['args'])
 
 def add_watcher(properties):
     properties['start'] = True
     return {'command': 'add', 'properties': properties}
 
 cc = CircusClient()
-o1 = cc.call(add_watcher(apicolat))
-o2 = cc.call(add_watcher(web))
+out = cc.call(add_watcher(apicolat))
+out_stats = cc.call(add_watcher(statistics))
+
+print(out)
 
 error_output = ''
-if o1['status'] == 'error':
-    error_output += o1['reason']
-if o2['status'] == 'error':
-    error_output += o2['reason']
+if out['status'] == 'error':
+    error_output += out['reason']
+if out_stats['status'] == 'error':
+    error_output += out_stats['reason']
 
 if error_output:
     print(error_output, file=sys.stderr)
